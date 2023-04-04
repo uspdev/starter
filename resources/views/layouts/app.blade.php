8567fc3be5c511a33c3b958410040d4eeea919f9
Masaki K Neto <kawabata@usp.br>
2023-04-04 10:57:17 -0300

    atualizando para theme 2.7.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,21 @@
 @extends('laravel-usp-theme::master')
 
-@section('title') 
-  @parent 
+{{-- Blocos do laravel-usp-theme --}}
+{{-- Ative ou desative cada bloco --}}
+
+{{-- Target:card-header; class:card-header-sticky --}}
+@include('laravel-usp-theme::blocos.sticky')
+
+{{-- Target: button, a; class: btn-spinner, spinner --}}
+@include('laravel-usp-theme::blocos.spinner')
+
+{{-- Target: table; class: datatable-simples --}}
+@include('laravel-usp-theme::blocos.datatable-simples')
+
+{{-- Fim de blocos do laravel-usp-theme --}}
+
+@section('title')
+  @parent
 @endsection
 
 @section('styles')
